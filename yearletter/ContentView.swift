94121af856c16fdbcce236d2b53fd3f17190107c
kimain050401 <kimain0401@gmail.com>
2024-06-17 18:04:13 +0900

    Initial Commit

---
+++ yearletter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yearletter
+//
+//  Created by 김아인 on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yearletter/yearletterApp.swift
@@ -0,0 +1,17 @@
+//
+//  yearletterApp.swift
+//  yearletter
+//
+//  Created by 김아인 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct yearletterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
