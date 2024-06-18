2d70b24da3231e770f355505816d296606fe1f16
kimain050401 <kimain0401@gmail.com>
2024-06-18 11:42:28 +0900

    Add Today Date and Question Text

---
+++ yearletter/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  yearletter
+//
+//  Created by 김아인 on 6/18/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
